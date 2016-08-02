0455f862f02d6dd06aad3b5262ff8082d2f3a0c7
Han Gao <ghnankai@qq.com>
2016-08-02 14:42:43 -0400

    Init

---
+++ src/com/slidingmenu/MainActivity.java
@@ -0,0 +1,40 @@
+package com.slidingmenu;
+
+import com.imooc.slidingmenu.R;
+import com.slidingmenu.view.SlidingMenu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.Window;
+
+public class MainActivity extends Activity
+{
+
+	private SlidingMenu mLeftMenu ; 
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+		
+		mLeftMenu = (SlidingMenu) findViewById(R.id.id_menu);
+	}
+	
+	public void toggleMenu(View view)
+	{
+		mLeftMenu.toggle();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
